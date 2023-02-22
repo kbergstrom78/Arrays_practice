a6ea6d1252b17924afb27231ac9ea6dbd5b02402
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-02-22 16:28:12 -0700

    Update syntax line 4

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,5 @@
 ## Arrays
 dogs = ["Chaco", "Kila", "Puck", "Oli"]
 kids_ages = [19, 17, 14, 13]
-sale prices = [19.99, 14.99, 9.99, 4.99]
+sale_prices = [19.99, 14.99, 9.99, 4.99]
 condition_met = [true, true, false, nil]
